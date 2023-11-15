c99a4058cd2d52282516f8e03d4a92a3a80d825c
craftablescience <me@craftablescience.info>
2023-11-14 21:32:18 -0500

    feat: better mdl preview mouse controls

--- src/gui/previews/MDLPreview.h
+++ src/gui/previews/MDLPreview.h
@@ -84,7 +84,7 @@ protected:
 
 	void mousePressEvent(QMouseEvent* event) override;
 
-	void mouseReleaseEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
 
 	void timerEvent(QTimerEvent* event) override;
 
