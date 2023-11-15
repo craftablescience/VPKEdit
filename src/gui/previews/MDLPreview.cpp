6b8b0d8742308f5ab1ee871752b367b090b24ae5
craftablescience <me@craftablescience.info>
2023-11-14 20:22:57 -0500

    fix: dont load alpha channel for textures on meshes, on linux the space behind the window bleeds through???

--- src/gui/previews/MDLPreview.cpp
+++ src/gui/previews/MDLPreview.cpp
@@ -280,7 +280,7 @@ static std::optional<VTFData> getTextureDataForMaterial(const VPK& vpk, const st
 
 	VTFLib::CVTFFile vtf;
 	vtf.Load(textureFile->data(), static_cast<vlUInt>(textureFile->size()), false);
-	return VTFDecoder::decodeImage(vtf, 1, 1, 0, true);
+	return VTFDecoder::decodeImage(vtf, 1, 1, 0, false);
 }
 
 void MDLPreview::setMesh(const QString& path, const VPK& vpk) const {
