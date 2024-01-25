267af3869ff17248f1d788ec5e80a792c6e3b23e
craftablescience <me@craftablescience.info>
2024-01-24 21:23:01 -0500

    feat(gui): add icons to entries in the entry tree

--- src/gui/EntryTree.cpp
+++ src/gui/EntryTree.cpp
@@ -1,18 +1,78 @@
 #include "EntryTree.h"
 
+#include <QApplication>
+#include <QFileInfo>
 #include <QKeyEvent>
 #include <QMessageBox>
+#include <QMimeDatabase>
+#include <QMimeType>
 #include <QProgressBar>
 #include <QStyle>
 #include <QThread>
 
+#include "previews/TextPreview.h"
 #include "EntryContextMenuData.h"
 #include "Window.h"
 
+#if defined(_WIN32)
+	#include <windows.h>
+#endif
+
 using namespace vpkedit;
+
+namespace {
+
+#ifdef _WIN32
+QIcon getIconForExtensionWin(const QString& extension) {
+	SHFILEINFO shellFileInfo;
+	ZeroMemory(&shellFileInfo, sizeof(SHFILEINFO));
+	if (SHGetFileInfo(extension.toStdWString().c_str(), FILE_ATTRIBUTE_NORMAL, &shellFileInfo, sizeof(SHFILEINFO), SHGFI_ICON | SHGFI_USEFILEATTRIBUTES) != ERROR_SUCCESS && shellFileInfo.hIcon) {
+		QIcon icon{QPixmap::fromImage(QImage::fromHICON(shellFileInfo.hIcon))};
+		DestroyIcon(shellFileInfo.hIcon);
+		return icon;
+	}
+	return {};
+}
+#endif
+
+const QIcon& getIconForExtension(QString extension) {
+	// Convert text extensions to .txt so they don't use the unknown icon
+	if (TextPreview::EXTENSIONS.contains(extension)) {
+		extension = ".txt";
+	}
+
+	// Memoize so we're not constantly searching for an icon that's already been found
+	static QMap<QString, QIcon> cachedExtensions;
+	if (cachedExtensions.contains(extension)) {
+		return cachedExtensions[extension];
+	}
+
+#if defined(_WIN32)
+	if (auto icon = ::getIconForExtensionWin(extension); !icon.isNull()) {
+		cachedExtensions[extension] = icon;
+		return cachedExtensions[extension];
+	}
+#endif
+	static QMimeDatabase mimeDatabase;
+	QList<QMimeType> mimeTypes = mimeDatabase.mimeTypesForFileName(extension);
+	for (const auto& mimeType : mimeTypes) {
+		if (auto icon = QIcon::fromTheme(mimeType.iconName()); !icon.isNull()) {
+			cachedExtensions[extension] = icon;
+			return cachedExtensions[extension];
+		}
+	}
+
+	static auto defaultExtensionIcon = QApplication::style()->standardIcon(QStyle::SP_FileIcon);
+	return defaultExtensionIcon;
+}
+
+} // namespace
 
 class EntryItem : public QTreeWidgetItem {
 public:
+	explicit EntryItem(QTreeWidget* parent)
+			: QTreeWidgetItem(parent) {}
+
 	explicit EntryItem(QTreeWidgetItem* parent)
 			: QTreeWidgetItem(parent) {}
 
@@ -92,8 +152,10 @@ EntryTree::EntryTree(Window* window_, QWidget* parent)
 }
 
 void EntryTree::loadVPK(VPK& vpk, QProgressBar* progressBar, const std::function<void()>& finishCallback) {
-    this->root = new QTreeWidgetItem(this);
+    this->root = new EntryItem(this);
     this->root->setText(0, vpk.getPrettyFilename().data());
+	// Set the icon now even though its set later, that way it will be present while loading
+	this->root->setIcon(0, this->style()->standardIcon(QStyle::SP_DirIcon));
 
     // Set up progress bar
     progressBar->setMinimum(0);
@@ -333,14 +395,14 @@ void EntryTree::addNestedEntryComponents(const QString& path) const {
     QStringList components = path.split('/', Qt::SkipEmptyParts);
     QTreeWidgetItem* currentItem = nullptr;
 
-    for (const auto& component : components) {
+    for (int i = 0; i < components.size(); i++) {
         QTreeWidgetItem* newItem = nullptr;
 
         // Find the child item with the current component text under the current parent item
         int childCount = currentItem ? currentItem->childCount() : this->root->childCount();
-        for (int i = 0; i < childCount; ++i) {
-            QTreeWidgetItem* childItem = currentItem ? currentItem->child(i) : this->root->child(i);
-            if (childItem->text(0) == component) {
+        for (int j = 0; j < childCount; j++) {
+            QTreeWidgetItem* childItem = currentItem ? currentItem->child(j) : this->root->child(j);
+            if (childItem->text(0) == components[i]) {
                 newItem = childItem;
                 break;
             }
@@ -353,7 +415,12 @@ void EntryTree::addNestedEntryComponents(const QString& path) const {
             } else {
                 newItem = new EntryItem(this->root);
             }
-            newItem->setText(0, component);
+            newItem->setText(0, components[i]);
+			if (i != components.size() - 1) { // is a directory
+				newItem->setIcon(0, this->style()->standardIcon(QStyle::SP_DirIcon));
+			} else {
+				newItem->setIcon(0, ::getIconForExtension("." + QFileInfo(components[i]).suffix()));
+			}
         }
 
         currentItem = newItem;
