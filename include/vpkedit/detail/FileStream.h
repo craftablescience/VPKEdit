ed6b95ecaf8b206220105946c514a7992dc708f4
craftablescience <me@craftablescience.info>
2024-01-27 02:50:49 -0500

    fix(lib): roll back bugged changes to FileStream

--- include/vpkedit/detail/FileStream.h
+++ include/vpkedit/detail/FileStream.h
@@ -10,53 +10,43 @@
 
 namespace vpkedit::detail {
 
+enum FileStreamOptions {
+	FILESTREAM_OPT_READ                  = 1 << 0,
+	FILESTREAM_OPT_WRITE                 = 1 << 1,
+	FILESTREAM_OPT_APPEND                = 1 << 2,
+	FILESTREAM_OPT_TRUNCATE              = 1 << 3,
+	FILESTREAM_OPT_CREATE_IF_NONEXISTENT = 1 << 4,
+};
+
 template<typename T>
 concept PODType = std::is_trivial_v<T> && std::is_standard_layout_v<T>;
 
-enum FileStreamOptions {
-    FILESTREAM_OPT_READ                  = 1 << 0,
-    FILESTREAM_OPT_WRITE                 = 1 << 1,
-    FILESTREAM_OPT_APPEND                = 1 << 2,
-    FILESTREAM_OPT_TRUNCATE              = 1 << 3,
-    FILESTREAM_OPT_CREATE_IF_NONEXISTENT = 1 << 4,
-};
-
 class FileStream {
 public:
-    explicit FileStream(const std::string& filepath, int options = FILESTREAM_OPT_READ);
-    FileStream(const FileStream& other) = delete;
-    FileStream& operator=(const FileStream& other) = delete;
-    FileStream(FileStream&& other) noexcept = default;
-    FileStream& operator=(FileStream&& other) noexcept = default;
+	explicit FileStream(const std::string& filepath, int options = FILESTREAM_OPT_READ);
+	FileStream(const FileStream& other) = delete;
+	FileStream& operator=(const FileStream& other) = delete;
+	FileStream(FileStream&& other) noexcept = default;
+	FileStream& operator=(FileStream&& other) noexcept = default;
 
-    explicit operator bool() const;
-    bool operator!() const;
+	explicit operator bool() const;
 
-	void seek_input(std::size_t offset, std::ios::seekdir offsetFrom = std::ios::beg);
+	void seekInput(std::uint64_t offset, std::ios::seekdir offsetFrom = std::ios::beg);
 
-	template<PODType T = std::byte>
-	void skip_input(std::size_t n = 1) {
-		if (!n) {
-			return;
-		}
-		this->seek_input(sizeof(T) * n, std::ios::cur);
+	[[nodiscard]] std::uint64_t tellInput();
+
+	void seekOutput(std::uint64_t offset, std::ios::seekdir offsetFrom = std::ios::beg);
+
+	[[nodiscard]] std::uint64_t tellOutput();
+
+	template<std::uint64_t L>
+	[[nodiscard]] std::array<std::byte, L> readBytes() {
+		std::array<std::byte, L> out;
+		this->streamFile.read(reinterpret_cast<char*>(out.data()), L);
+		return out;
 	}
 
-	[[nodiscard]] std::size_t tell_input();
-
-	void seek_output(std::size_t offset, std::ios::seekdir offsetFrom = std::ios::beg);
-
-	template<PODType T = std::byte>
-	void skip_output(std::size_t n = 1) {
-		if (!n) {
-			return;
-		}
-		this->seek_output(sizeof(T) * n, std::ios::cur);
-	}
-
-	[[nodiscard]] std::size_t tell_output();
-
-	[[nodiscard]] std::byte peek(long offset = 0);
+	[[nodiscard]] std::vector<std::byte> readBytes(std::uint64_t length);
 
 	template<PODType T>
 	[[nodiscard]] T read() {
@@ -65,52 +55,12 @@ public:
 		return obj;
 	}
 
-	template<std::size_t L>
-	[[nodiscard]] std::array<std::byte, L> read_bytes() {
-		std::array<std::byte, L> out;
-		this->streamFile.read(reinterpret_cast<char*>(out.data()), L);
-		return out;
-	}
-
-	[[nodiscard]] std::vector<std::byte> read_bytes(std::size_t length);
-
-	[[nodiscard]] std::string read_string();
-
-	[[nodiscard]] std::string read_string(std::size_t n, bool stopOnNullTerminator = true);
-
 	template<PODType T>
 	void read(T& obj) {
 		this->streamFile.read(reinterpret_cast<char*>(&obj), sizeof(T));
 	}
 
-	template<PODType T, std::size_t N>
-	void read(T(&obj)[N]) {
-		for (int i = 0; i < N; i++) {
-			obj[i] = this->read<T>();
-		}
-	}
-
-	template<PODType T, std::size_t N>
-	void read(std::array<T, N>& obj) {
-		for (int i = 0; i < N; i++) {
-			obj[i] = this->read<T>();
-		}
-	}
-
-	template<PODType T>
-	void read(std::vector<T>& obj, std::size_t n) {
-		obj.clear();
-		if (!n) {
-			return;
-		}
-		obj.reserve(n);
-		for (int i = 0; i < n; i++) {
-			obj.push_back(this->read<T>());
-		}
-	}
-
 	void read(std::string& obj) {
-		obj.clear();
 		char temp = this->read<char>();
 		while (temp != '\0') {
 			obj += temp;
@@ -118,69 +68,36 @@ public:
 		}
 	}
 
-	void read(std::string& obj, std::size_t n, bool stopOnNullTerminator = true) {
-		obj.clear();
-		if (!n) {
-			return;
-		}
-		obj.reserve(n);
-		for (int i = 0; i < n; i++) {
-			char temp = this->read<char>();
-			if (temp == '\0' && stopOnNullTerminator) {
-				// Read the required number of characters and exit
-				this->skip_input<char>(n - i - 1);
-				break;
-			}
-			obj += temp;
-		}
+	template<std::uint64_t L>
+	void writeBytes(const std::array<std::byte, L> obj) {
+		this->streamFile.write(reinterpret_cast<const char*>(obj.data()), L);
 	}
+
+	void writeBytes(const std::vector<std::byte>& buffer);
 
 	template<PODType T>
 	void write(T obj) {
-		this->streamFile.write(reinterpret_cast<const char*>(&obj), sizeof(T));
-	}
-
-	template<std::size_t L>
-	void write_bytes(const std::array<std::byte, L>& buffer) {
-		this->streamFile.write(reinterpret_cast<const char*>(buffer.data()), L);
-	}
-
-	void write_bytes(const std::vector<std::byte>& buffer);
-
-	template<PODType T, std::size_t N>
-	void write(T(&obj)[N]) {
-		for (int i = 0; i < N; i++) {
-			this->write(obj[i]);
-		}
-	}
-
-	template<PODType T, std::size_t N>
-	void write(std::array<T, N>& obj) {
-		for (int i = 0; i < N; i++) {
-			this->write(obj[i]);
-		}
+		this->write(&obj);
 	}
 
 	template<PODType T>
-	void write(const std::vector<T>& obj) {
-		for (int i = 0; i < obj.size(); i++) {
-			this->write(obj[i]);
-		}
+	void write(T* obj) {
+		this->streamFile.write(reinterpret_cast<const char*>(obj), sizeof(T));
 	}
 
 	template<PODType T>
-	void write_unsafe(T* obj, std::size_t len) {
+	void write(T* obj, std::size_t len) {
 		this->streamFile.write(reinterpret_cast<const char*>(obj), sizeof(T) * len);
 	}
 
-	void write(std::string_view obj) {
+	void write(const std::string& obj) {
 		this->streamFile.write(obj.data(), static_cast<std::streamsize>(obj.size()));
 	}
 
 	void flush();
 
 protected:
-    std::fstream streamFile;
+	std::fstream streamFile;
 };
 
 } // namespace vpkedit::detail
--- src/lib/detail/FileStream.cpp
+++ src/lib/detail/FileStream.cpp
@@ -5,77 +5,57 @@
 using namespace vpkedit::detail;
 
 FileStream::FileStream(const std::string& filepath, int options) {
-    if ((options & FILESTREAM_OPT_CREATE_IF_NONEXISTENT) && !std::filesystem::exists(filepath)) {
-        std::ofstream create(filepath, std::ios::trunc);
-    }
-    std::ios::openmode openMode = std::ios::binary;
-    if (options & FILESTREAM_OPT_READ) {
-        openMode |= std::ios::in;
-    }
-    if (options & FILESTREAM_OPT_WRITE) {
-        openMode |= std::ios::out;
-    }
+	if ((options & FILESTREAM_OPT_CREATE_IF_NONEXISTENT) && !std::filesystem::exists(filepath)) {
+		std::ofstream create(filepath, std::ios::trunc);
+	}
+	std::ios::openmode openMode = std::ios::binary;
+	if (options & FILESTREAM_OPT_READ) {
+		openMode |= std::ios::in;
+	}
+	if (options & FILESTREAM_OPT_WRITE) {
+		openMode |= std::ios::out;
+	}
 	if (options & FILESTREAM_OPT_APPEND) {
 		openMode |= std::ios::app;
 	}
-    if (options & FILESTREAM_OPT_TRUNCATE) {
-        openMode |= std::ios::trunc;
-    }
-    this->streamFile.open(filepath, openMode);
-    this->streamFile.unsetf(std::ios::skipws);
+	if (options & FILESTREAM_OPT_TRUNCATE) {
+		openMode |= std::ios::trunc;
+	}
+	this->streamFile.open(filepath, openMode);
+	this->streamFile.unsetf(std::ios::skipws);
 }
 
 FileStream::operator bool() const {
-    return static_cast<bool>(this->streamFile);
+	return static_cast<bool>(this->streamFile);
 }
 
-bool FileStream::operator!() const {
-    return !this->operator bool();
+void FileStream::seekInput(std::uint64_t offset, std::ios::seekdir offsetFrom) {
+	this->streamFile.seekg(static_cast<long>(offset), offsetFrom);
 }
 
-void FileStream::seek_input(std::uint64_t offset, std::ios::seekdir offsetFrom) {
-    this->streamFile.seekg(static_cast<long>(offset), offsetFrom);
+std::uint64_t FileStream::tellInput() {
+	return this->streamFile.tellg();
 }
 
-std::size_t FileStream::tell_input() {
-    return this->streamFile.tellg();
+void FileStream::seekOutput(std::uint64_t offset, std::ios::seekdir offsetFrom) {
+	this->streamFile.seekp(static_cast<long>(offset), offsetFrom);
 }
 
-void FileStream::seek_output(std::uint64_t offset, std::ios::seekdir offsetFrom) {
-    this->streamFile.seekp(static_cast<long>(offset), offsetFrom);
+std::uint64_t FileStream::tellOutput() {
+	return this->streamFile.tellp();
 }
 
-std::size_t FileStream::tell_output() {
-    return this->streamFile.tellp();
-}
-
-std::byte FileStream::peek(long offset) {
-	return static_cast<std::byte>(this->streamFile.peek());
-}
-
-std::vector<std::byte> FileStream::read_bytes(std::uint64_t length) {
-    std::vector<std::byte> out;
-    out.resize(length);
-    this->streamFile.read(reinterpret_cast<char*>(out.data()), static_cast<std::streamsize>(length));
-    return out;
-}
-
-std::string FileStream::read_string() {
-	std::string out;
-	this->read(out);
+std::vector<std::byte> FileStream::readBytes(std::uint64_t length) {
+	std::vector<std::byte> out;
+	out.resize(length);
+	this->streamFile.read(reinterpret_cast<char*>(out.data()), static_cast<std::streamsize>(length));
 	return out;
 }
 
-std::string FileStream::read_string(std::size_t n, bool stopOnNullTerminator) {
-	std::string out;
-	this->read(out, n, stopOnNullTerminator);
-	return out;
-}
-
-void FileStream::write_bytes(const std::vector<std::byte>& buffer) {
-    this->streamFile.write(reinterpret_cast<const char*>(buffer.data()), static_cast<std::streamsize>(buffer.size()));
+void FileStream::writeBytes(const std::vector<std::byte>& buffer) {
+	this->streamFile.write(reinterpret_cast<const char*>(buffer.data()), static_cast<std::streamsize>(buffer.size()));
 }
 
 void FileStream::flush() {
-    this->streamFile.flush();
+	this->streamFile.flush();
 }
