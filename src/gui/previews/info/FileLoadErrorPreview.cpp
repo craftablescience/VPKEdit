e8137b3d4e9b70e9cce84e8e4c79751213bb6bb0
craftablescience <me@craftablescience.info>
2024-02-09 09:31:02 -0500

    fix(gui): update file load error message to not refer specifically to a VPK

--- src/gui/previews/info/FileLoadErrorPreview.cpp
+++ src/gui/previews/info/FileLoadErrorPreview.cpp
@@ -1,4 +1,4 @@
 #include "FileLoadErrorPreview.h"
 
 FileLoadErrorPreview::FileLoadErrorPreview(QWidget* parent)
-        : AbstractInfoPreview({":/error.png"}, tr("Failed to read file contents!\nPlease ensure that a game or another application is not using the VPK."), parent) {}
+        : AbstractInfoPreview({":/error.png"}, tr("Failed to read file contents!\nPlease ensure that a game or another application is not using the file."), parent) {}
