9eaf5abd14ae3330bad67e4f6ba7563bb148cd66
craftablescience <me@craftablescience.info>
2024-02-19 10:12:12 -0500

    chore(gui): remove redundant english localization file

--- src/gui/_gui.cmake
+++ src/gui/_gui.cmake
@@ -110,7 +110,6 @@ endif()
 add_executable(${PROJECT_NAME} WIN32 ${${PROJECT_NAME}_SOURCES})
 
 qt_add_translations(${PROJECT_NAME}
-        TS_FILES "${CMAKE_CURRENT_LIST_DIR}/res/i18n/${PROJECT_NAME}.en.ts"
         TS_FILES "${CMAKE_CURRENT_LIST_DIR}/res/i18n/${PROJECT_NAME}.ru.ts"
         SOURCES ${${PROJECT_NAME}_SOURCES})
 
