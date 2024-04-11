41cf49403bbbd0a3559bfe72314c74852200f338
craftablescience <me@craftablescience.info>
2024-04-11 16:49:01 -0400

    feat(gui): remember save directory between create vpk from folder prompts (closes #116)

--- src/gui/Window.cpp
+++ src/gui/Window.cpp
@@ -440,7 +440,7 @@ void Window::newVPK(bool fromDirectory, const QString& startPath) {
         return;
     }
 
-    auto vpkPath = QFileDialog::getSaveFileName(this, tr("Save New VPK"), fromDirectory ? QString(std::filesystem::path(dirPath.toStdString()).stem().string().c_str()) + (singleFile || dirPath.endsWith("_dir") ? ".vpk" : "_dir.vpk") : startPath, VPK_SAVE_FILTER);
+    auto vpkPath = QFileDialog::getSaveFileName(this, tr("Save New VPK"), fromDirectory ? QString(((std::filesystem::path(dirPath.toStdString()).parent_path().string() + std::filesystem::path::preferred_separator + std::filesystem::path(dirPath.toStdString()).stem().string()) + (singleFile || dirPath.endsWith("_dir") ? ".vpk" : "_dir.vpk")).c_str()) : startPath, VPK_SAVE_FILTER);
     if (vpkPath.isEmpty()) {
         return;
     }
