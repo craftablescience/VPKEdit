602b8cefb8f14c9e0bb7e407b143929d139db481
craftablescience <me@craftablescience.info>
2024-04-19 21:29:41 -0400

    feat(lib): use cryptopp for Adler32 implementation

--- include/vpkedit/detail/Adler32.h
+++ include/vpkedit/detail/Adler32.h
@@ -6,8 +6,8 @@
 
 namespace vpkedit::detail {
 
-std::uint32_t computeAdler32(const std::vector<std::byte>& buffer, std::uint32_t adler = 0);
+std::uint32_t computeAdler32(const std::vector<std::byte>& buffer);
 
-std::uint32_t computeAdler32(const std::byte* buffer, std::size_t len, std::uint32_t adler = 0);
+std::uint32_t computeAdler32(const std::byte* buffer, std::size_t len);
 
 } // namespace vpkedit::detail
