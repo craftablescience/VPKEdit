16eb77cee06d8191786e90d700bdb6f7b55c5689
craftablescience <me@craftablescience.info>
2024-05-26 22:16:16 -0400

    feat(installer): use zstd compression for deb file

--- src/installer/_installer.cmake
+++ src/installer/_installer.cmake
@@ -161,6 +161,7 @@ else()
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR} <${CPACK_PACKAGE_CONTACT}>")
     set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/${PROJECT_NAME}")
     set(CPACK_DEBIAN_PACKAGE_DEPENDS "libxcb-cursor0")
+    set(CPACK_DEBIAN_COMPRESSION_TYPE "zstd")
 
     # Add symlinks so it can be ran from anywhere
     if(VPKEDIT_BUILD_CLI)
