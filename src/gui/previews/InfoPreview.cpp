11d90dd5ffe0c25c24de5471dd78d18082e55b8e
underscorejoser <259754904+underscorejoser@users.noreply.github.com>
2026-02-24 11:41:08 -0300

    fix(gui): scale with smooth mode

--- src/gui/previews/InfoPreview.cpp
+++ src/gui/previews/InfoPreview.cpp
@@ -11,7 +11,7 @@ InfoPreview::InfoPreview(const QPixmap& icon, const QString& text, QWidget* pare
 	layout->setContentsMargins(0,0,0,0);
 
 	this->image = new QLabel(this);
-	this->image->setPixmap(icon.scaled(64, 64, Qt::KeepAspectRatio));
+	this->image->setPixmap(icon.scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 	layout->addWidget(this->image, 0, Qt::AlignHCenter | Qt::AlignBottom);
 
 	this->error = new QLabel(this);
@@ -24,6 +24,6 @@ InfoPreview::InfoPreview(QWidget* parent)
 		: InfoPreview({":/icons/warning.png"}, "", parent) {}
 
 void InfoPreview::setData(const QPixmap& icon, const QString& text) {
-	this->image->setPixmap(icon.scaled(64, 64, Qt::KeepAspectRatio));
+	this->image->setPixmap(icon.scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 	this->error->setText(text);
 }
